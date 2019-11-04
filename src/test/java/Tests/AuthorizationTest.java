acaf0f6bbab2172793b18b67b2e23541d1910f37
pugpiece <bruskovod@gmail.com>
2019-11-04 13:39:50 +0400

    Commit 1

---
+++ src/test/java/Tests/AuthorizationTest.java
@@ -0,0 +1,30 @@
+package Tests;
+
+import Methods.TestMethods;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AuthorizationTest {
+    private ChromeDriver driver;
+    private String url;
+
+    @Before
+    public void setUp()
+    {
+        System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        url = "https://beru.ru";
+    }
+
+    @Test
+    public void Test() throws InterruptedException {
+
+        TestMethods authorize = new TestMethods();
+        authorize.OpenBeru(driver,url);
+        authorize.Authorise(driver);
+        authorize.CheckAuthorization(driver);
+
+    }
+}
